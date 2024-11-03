25b90656ee7f44276b5fd8511fc9fc33e5cdad98
Osvetamp4 <82180979+Osvetamp4@users.noreply.github.com>
2024-11-02 22:47:48 -0400

    update

--- Scripts/json-to-image.js
+++ Scripts/json-to-image.js
@@ -1,17 +1,17 @@
-var fs = require('fs'),
-    fabric = require('fabric').fabric,
-    out = fs.createWriteStream(__dirname + '/output.png');
+// var fs = require('fs'),
+//     fabric = require('fabric').fabric,
+//     out = fs.createWriteStream(__dirname + '/output.png');
 
-let jsonstr = fs.readFileSync(__dirname + '/json/test_json.json', 'utf-8');
-let json = JSON.parse(jsonstr);
+// let jsonstr = fs.readFileSync(__dirname + '/json/test_json.json', 'utf-8');
+// let json = JSON.parse(jsonstr);
 
-var canvas = new fabric.StaticCanvas(null, { width: 500, height: 500 });
+// var canvas = new fabric.StaticCanvas(null, { width: 500, height: 500 });
 
-canvas.loadFromJSON(json, function() {
-    canvas.renderAll();
-    var stream = canvas.createPNGStream();
-    stream.pipe(out);
-    out.on('finish', function () {
-        // do something here
-    });
-});
+// canvas.loadFromJSON(json, function() {
+//     canvas.renderAll();
+//     var stream = canvas.createPNGStream();
+//     stream.pipe(out);
+//     out.on('finish', function () {
+//         // do something here
+//     });
+// });
